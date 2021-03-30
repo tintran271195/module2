d056758139d2678cf0c383bd2ac83334bb21ddc8
tintran271195 <tintran271195@gmail.com>
2021-03-30 16:15:45 +0700

    Linear Equation Resolver

---
+++ LinearEquationResolver/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        double a = sc.nextDouble();
+        System.out.println("Nhap b: ");
+        double b = sc.nextDouble();
+        System.out.println("Nhap c: ");
+        double c = sc.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
