cb793c3433ba6f73d7028baef0b06069bdcb9ee3
tintran271195 <tintran271195@gmail.com>
2021-03-30 15:20:21 +0700

    SystemTime

---
+++ SystemTime/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Date now = new Date();
+
+        System.out.println("Date now: " + now );
+    }
+}
