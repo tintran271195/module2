ec39cf17ec9844842f54306069dfa6342dd317f1
tintran271195 <tintran271195@gmail.com>
2021-03-31 09:23:17 +0700

    Bmi

---
+++ Bmi/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	double height, weight, bmi;
+        System.out.print("Enter Your Weight(in Kilogram): ");
+        weight = sc.nextDouble();
+        System.out.print("Enter Your Height(in Meter): ");
+        height = sc.nextDouble();
+        bmi = weight/Math.pow(height,2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+        if ( bmi < 18 )
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+            else if (bmi < 25.0)
+                System.out.printf("%-20.2f%s", bmi, "Normal");
+            else if (bmi < 30.0)
+                System.out.printf("%-20.2f%s", bmi, "Overweight");
+            else
+                System.out.printf("%-20.2f%s", bmi, "Obese");
+        }
+    }
+
