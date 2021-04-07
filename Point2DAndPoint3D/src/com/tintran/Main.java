30f433a7ee08bf704af6901ce8f459f2dcb6fb2a
tintran271195 <tintran271195@gmail.com>
2021-04-07 17:05:44 +0700

    Point2D and 3D

---
+++ Point2DAndPoint3D/src/com/tintran/Main.java
@@ -0,0 +1,10 @@
+package com.tintran;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Point2D a = new Point2D(2,3);
+	Point3D b = new Point3D(2,3,4);
+        System.out.println(b.toString());
+    }
+}
